37cac38e6f0274214cd1c67b08374b8ea47c266c
diadatp <git@diadatp.com>
2023-08-28 22:06:11 +0530

    Add the ability to change tone_gen's div at runtime.

--- src/tone_gen.v
+++ src/tone_gen.v
@@ -1,14 +1,14 @@
 `default_nettype none
 
 // This module generates a square wave with a period
-// 2*MAX_COUNT times that of the input clock.
+// 2*div times that of the input clock.
 
 module tone_gen #(
-    parameter MAX_COUNT = 5,
     parameter WIDTH_COUNTER = 10
 ) (
     input clk,
     input rst,
+    input [WIDTH_COUNTER-1:0] div,
     output reg tone
 );
 
@@ -18,8 +18,8 @@ module tone_gen #(
     if (rst) begin
       count <= 0;
       tone  <= 0;
-    end else if (count == (MAX_COUNT - 1)) begin
-      count <= 0;
+    end else if (count == div) begin
+      count <= 1;
       tone  <= ~tone;
     end else begin
       count <= count + 1'b1;
