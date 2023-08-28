938b9f5491e39cb48dc74e1377495923a9101b0e
diadatp <git@diadatp.com>
2023-08-28 21:59:57 +0530

    Add priority decoder and octave selection.

--- src/tt_um_simplepiano.v
+++ src/tt_um_simplepiano.v
@@ -11,31 +11,80 @@ module tt_um_simplepiano (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // 12 nets will form an octave from
-  // the output of the tone generators
-  wire [11:0] notes;
+  // localparam SIMULTANEOUS_NOTES = 8;
 
-  // 12 notes from the 4th octave, C to B
-  // notes are calculated wrt A4 at 440Hz
-  genvar i;
-  generate
-    for (i = 0; i < 12; i = i + 1) begin
-      tone_gen #(
-          .MAX_COUNT(440 * $pow($pow(2, 1.0 / 12), i - 9)),
-          .WIDTH_COUNTER(10)
-      ) note (
-          .clk (clk),
-          .rst (~rst_n),
-          .tone(notes[i])
-      );
-    end
-  endgenerate
+  // reg note_output;
 
-  assign uo_out = notes[7:0];
-  assign uio_out[3:0] = notes[11:8];
+  // do decoding 12bit user_keys - > 4bit note
+  // and key priority
+
+  wire [11:0] user_keys;
+  assign user_keys = {uio_in[3:0], ui_in};
+
+  reg [3:0] note_decoded;
+  always @(posedge clk) begin
+    casex (user_keys)
+      12'b1xxx_xxxx_xxxx: note_decoded = 4'd0;
+      12'b01xx_xxxx_xxxx: note_decoded = 4'd1;
+      12'b001x_xxxx_xxxx: note_decoded = 4'd2;
+      12'b0001_xxxx_xxxx: note_decoded = 4'd3;
+      12'b0000_1xxx_xxxx: note_decoded = 4'd4;
+      12'b0000_01xx_xxxx: note_decoded = 4'd5;
+      12'b0000_001x_xxxx: note_decoded = 4'd6;
+      12'b0000_0001_xxxx: note_decoded = 4'd7;
+      12'b0000_0000_1xxx: note_decoded = 4'd8;
+      12'b0000_0000_01xx: note_decoded = 4'd9;
+      12'b0000_0000_001x: note_decoded = 4'd10;
+      12'b0000_0000_0001: note_decoded = 4'd11;
+      12'b0000_0000_0000: note_decoded = 4'b1111;
+    endcase
+  end
+
+  // // 12 nets will form an octave from
+  // // the output of the tone generators
+  // wire [11:0] notes;
+
+  // // 12 notes from the 4th octave, C to B
+  // // notes are calculated wrt A4 at 440Hz
+  // genvar i;
+  // generate
+  //   for (i = 0; i < 12; i = i + 1) begin
+  //     tone_gen #(
+  //         .MAX_COUNT($rtoi(1000000 / (440 * $pow($pow(2, 1.0 / 12), i - 9)))),
+  //         .WIDTH_COUNTER(16)
+  //     ) note (
+  //         .clk (clk),
+  //         .rst (~rst_n),
+  //         .tone(notes[i])
+  //     );
+  //   end
+  // endgenerate
+
+
+
+  wire [15:0] div;
+
+  note_lut note_lut_dut (
+      .clk(clk),
+      .rstn(rst_n),
+      .note(note_decoded),
+      .octave(uio_in[7:4]),
+      .div(div)
+  );
+
+  tone_gen #(
+      .WIDTH_COUNTER(16)
+  ) tone_gen_1 (
+      .clk (clk),
+      .rst (~rst_n),
+      .div (div),
+      .tone(uo_out[0])
+  );
+
+  assign uo_out[7:1] = 0;
 
   // set the bidir signals to output mode and drive unused wires with 0 for now
-  assign uio_oe = 8'b1111_1111;
-  assign uio_out[7:4] = 4'b0000;
+  assign uio_oe = 8'b1111_0000;
+  assign uio_out = 0;
 
 endmodule
